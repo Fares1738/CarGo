99f6b64527e90c927c91fa5409955383468df3e2
Cargo <ahmednafisbd15@gmail.com>
2023-01-05 14:39:03 +0800

    nafis

--- lib/selfie_page.dart
+++ lib/selfie_page.dart
@@ -16,49 +16,9 @@ class _SelfiePageState extends State<SelfiePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-        children: [ Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Image.asset(
-              'assets/profile.png',
-              width: 270,
-              height: 200,
-            ),
-            Text(
-              'Complete Your Profile',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 120),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(200, 50),
-                      textStyle: TextStyle(fontSize: 21),
-                      backgroundColor: Colors.purple,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                    ),
-                    child: Text('VERIFY PROFILE'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => VerifyID()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            )
-          ]),
-        ),
-      ),
+        children: [
+        widget,
+              ]),
     );
   }
 }
