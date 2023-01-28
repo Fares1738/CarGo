2c5f599906281c30b725157b8fee34eee31d195b
cargo <mohamed2003torky@gmail.com>
2023-01-28 14:58:31 +0800

    Authentication done

---
+++ lib/services/database.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  //Collection Reference
+
+  final CollectionReference carCollection =
+      FirebaseFirestore.instance.collection('cars');
+}
